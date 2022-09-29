ac0bcf486c32493bcf4d551324941de062c79425
Bruno Marques Caroba <cocacola_bruno@hotmail.com>
2022-09-29 16:18:27 -0300

    version bump

---
+++ src/Crucible.js
@@ -0,0 +1,14 @@
+import './Crucible.css';
+import Home from "./pages/home/Home";
+import Menu from "./pages/menu/Menu";
+
+function Crucible() {
+  return (
+    <div className="App">
+        <Menu/>
+        <Home/>
+    </div>
+  );
+}
+
+export default Crucible;
